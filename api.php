9a0931a2d0e2db38a60a9dc715f83db9760b9413
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2024-02-12 21:29:42 +0600

    refactor api endpoints

--- api.php
+++ api.php
@@ -9,11 +9,11 @@ use Mehedi\WPQueryBuilder\Query\Join;
 use Mehedi\WPQueryBuilder\Relations\WithOne;
 use Mehedi\WPQueryBuilderExt\Relations\WithTaxonomy;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (! defined('ABSPATH')) {
+    exit;
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 class BookReaderController extends WP_REST_Controller
 {
@@ -21,105 +21,114 @@ class BookReaderController extends WP_REST_Controller
 
     public function register_routes()
     {
-		register_rest_route($this->namespace, '/books', [
-			'methods' => 'GET',
-			'callback' => [$this, 'index']
-		]);
+        register_rest_route($this->namespace, '/books', [
+            'methods' => 'GET',
+            'callback' => [$this, 'index'],
+        ]);
 
-	    register_rest_route($this->namespace, '/(?P<taxonomy>authors|categories)', [
-		    'methods' => 'GET',
-		    'callback' => [$this, 'taxonomy']
-	    ]);
+        register_rest_route($this->namespace, '/(?P<taxonomy>authors|categories)', [
+            'methods' => 'GET',
+            'callback' => [$this, 'taxonomies'],
+        ]);
+
+        register_rest_route($this->namespace, '/taxonomies/(?P<id>[\\d]+)', [
+            'methods' => 'GET',
+            'callback' => [$this, 'taxonomy'],
+        ]);
     }
 
+    public function index(WP_REST_Request $request)
+    {
+        $s = $request->get_param('s');
+        $termId = $request->get_param('termId');
+        $perPage = $request->get_param('perPage') ?? 20;
+        $page = $request->get_param('page') ?? 1;
 
-	public function index(WP_REST_Request $request) {
-		$limit = $request->get_param('limit') ?? 10;
-		$lastId = $request->get_param('lastId');
-		$s = $request->get_param('s');
-		$termId = $request->get_param('termId');
+        $query = DB::table('posts')
+            ->where('post_type', 'books')
+            ->where('post_status', 'publish')
+            ->withRelation(new WithTaxonomy('authors'), function (WithTaxonomy /** @var Builder $relation */ $relation) {
+                $relation->taxonomy('author');
+            })
+            ->withOne('thumbnail', function (WithOne $thumbnailQuery) {
+                $thumbnailQuery->from('posts')->where('post_type', 'attachment');
+            }, 'post_parent')
+            ->skip(($page - 1) * $perPage)
+            ->limit($perPage);
 
+        if ($s) {
+            $query->where('post_title', 'like', "%$s%");
+        }
 
-		$query = DB::table('posts')
-		          ->where('post_type', 'books')
-		          ->where('post_status', 'publish')
-		          ->withRelation(new WithTaxonomy('authors'), function (WithTaxonomy /** @var Builder $relation*/ $relation) {
-					  $relation->taxonomy('author');
-				  })
-		          ->withOne('thumbnail', function ( WithOne $thumbnailQuery) {
-						$thumbnailQuery->from('posts')->where('post_type', 'attachment');
-				}, 'post_parent')
-				->limit($limit);
+        if ($termId) {
+            $query->join('term_relationships', 'posts.ID', '=', 'term_relationships.object_id')
+                ->join('term_taxonomy', function (Join $join) use ($termId) {
+                    $join->on('term_relationships.term_taxonomy_id', '=', 'term_taxonomy.term_taxonomy_id')
+                        ->where('term_taxonomy.term_id', $termId);
+                });
+        }
 
-		if ($lastId) {
-			$query->where('ID', '<', $lastId);
-		}
+        return rest_ensure_response([
+            'data' => array_map(function ($post) {
+                return [
+                    'id' => $post->ID,
+                    'title' => $post->post_title,
+                    'authors' => array_map(function ($author) {
+                        return [
+                            'id' => $author->term_id,
+                            'name' => $author->name,
+                        ];
+                    }, $post->authors),
+                    'thumbnail' => $post->thumbnail->guid ?? null,
+                ];
+            }, $query->get()),
+        ]);
+    }
 
-		if ($s) {
-			$query->where('post_title', 'like', "%$s%");
-		}
+    public function taxonomies(WP_REST_Request $request)
+    {
+        $search = $request->get_param('s');
+        $taxonomy_type = $request->get_param('taxonomy') === 'authors' ? 'author' : 'category';
 
-		if ($termId) {
-			$query->join('term_relationships', 'posts.ID', '=', 'term_relationships.object_id')
-			      ->join('term_taxonomy', function ( Join $join) use($termId) {
-					  $join->on('term_relationships.term_taxonomy_id', '=', 'term_taxonomy.term_taxonomy_id')
-					       ->where('term_taxonomy.term_id', $termId);
-			      });
-		}
+        $query = DB::table('term_taxonomy')
+            ->join('terms', 'term_taxonomy.term_id', '=', 'terms.term_id')
+            ->where('taxonomy', $taxonomy_type)
+            ->where('count', '>', 0)
+            ->orderBy('name');
 
-		$data = $query->get();
+        if ($search) {
+            $query->where('name', 'like', "%$search%");
+        }
 
+        return [
+            'data' => array_map(function ($item) {
+                return [
+                    'id' => $item->term_id,
+                    'name' => $item->name,
+                    'slug' => $item->slug,
+                    'count' => $item->count,
+                ];
+            }, $query->get()),
+        ];
+    }
 
-		return rest_ensure_response([
-			'data' => array_map(function ($post) {
-				return [
-					'id' => $post->ID,
-					'title' => $post->post_title,
-					'authors' => array_map(function ($author) {
-						return [
-							'id' => $author->term_id,
-							'name' => $author->name
-						];
-					}, $post->authors),
-					'thumbnail' => $post->thumbnail->guid ?? null
-				];
-			}, $data)
-		]);
-	}
+    public function taxonomy(WP_REST_Request $request)
+    {
+        $termId = $request->get_param('id');
+        $item = DB::table('terms')->where('term_id', $termId)->first();
 
-	public function taxonomy(WP_REST_Request $request) {
-		$taxonomy_type = $request->get_param('taxonomy') === 'authors' ? 'author' : "category";
-		$search = $request->get_param('s');
-		$id = $request->get_param('id');
+        if (! $item) {
+            return new WP_REST_Response(['message' => 'No term found!'], 404);
+        }
 
-		$query = DB::table('term_taxonomy')
-					->join('terms', 'term_taxonomy.term_id', '=', 'terms.term_id')
-					->where('taxonomy', $taxonomy_type)
-					->where('count', '>', 0)
-					->orderBy('name');
-		if ($search) {
-			$query->where('name', 'like', "%$search%");
-		}
-
-		if ($id) {
-			$query->where('terms.term_id', $id);
-		}
-
-		$data = $query->get();
-
-		$items = array_map(function ($item) {
-			return [
-				'id' => $item->term_id,
-				'name' => $item->name,
-				'slug' => $item->slug,
-				'count' => $item->count
-			];
-		}, $data);
-
-		return [
-			'data' => $id && !empty($items) ? $items[0] : $items
-		];
-	}
+        return [
+            'data' => [
+                'id' => $item->term_id,
+                'name' => $item->name,
+                'slug' => $item->slug,
+            ],
+        ];
+    }
 }
 
-add_action( 'rest_api_init', [new BookReaderController(), 'register_routes']);
+add_action('rest_api_init', [new BookReaderController(), 'register_routes']);
